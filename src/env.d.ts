b9d4078a875b59a248e34e4356a79cebfc058344
Faddah Wolf <my_biz@me.com>
2024-09-23 15:30:46 -0700

    Added interfaces for ITEMS_PER_PAGE in .env.

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,9 @@
 /// <reference path="../.astro/types.d.ts" />
+interface ImportMetaEnv {
+	readonly ITEMS_PER_PAGE: number;
+	// more env variables...
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
