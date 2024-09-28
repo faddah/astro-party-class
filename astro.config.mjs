3134d386cbda12d17d7f3bac568a1c089831ca61
Faddah Wolf <my_biz@me.com>
2024-09-28 10:09:38 -0700

    db added to integration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+import db from '@astrojs/db';
+
 // https://astro.build/config
 export default defineConfig({
-	output: "hybrid",
-  integrations: [react()],
-	experimental: {
-		serverIslands: true,
-	}
+    output: "hybrid",
+  integrations: [react(), db()],
+    experimental: {
+        serverIslands: true,
+    }
 });
