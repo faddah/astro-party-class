2575d9ca21cc348b56affcfc83e4803e9178fb55
Faddah Wolf <my_biz@me.com>
2024-10-04 20:09:39 -0700

    Deleted this code as it was refactored above.

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -35,9 +35,7 @@ export const server: Server = {
 				} else if (input.text.length < 3) {
 					throw new Error("Please provide a longer text for the idea");
 				}
-				if (!input.good) {
-					throw new Error("Please choose the Radio Button for if this is a Good or Bad Idea.");
-				}
+
 				await db.insert(Idea).values(input as { text: string; good: boolean });
 
 				return `Success! New ${input.good ? 'good' : 'bad'} idea added!`;
