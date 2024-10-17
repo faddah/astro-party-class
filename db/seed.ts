ae950a7a25f177c34350f7b6b7478bc5d4fbe6c1
Faddah Wolf <my_biz@me.com>
2024-10-16 19:41:48 -0700

    Refactored seed.ts to use Turso for DB.

--- db/seed.ts
+++ db/seed.ts
@@ -1,9 +1,24 @@
-import { db, Idea } from 'astro:db';
+import { turso } from '../src/turso';
 
+export default async function seed() {
+	await turso.execute(`
+		INSERT INTO Idea IF NOT EXISTS (id, text, good) values
+		(1, 'Set up a dinner for friends', true),
+	`);
+	await turso.execute(`
+		INSERT INTO Idea IF NOT EXISTS (id, text, good) values
+		(2, 'Eat an entire block of cheese', false)
+		`);
+}
+
+// import { db, Idea } from 'astro:db';
+
+/* 
 // https://astro.build/db/seed
 export default async function seed() {
 	await db.insert(Idea).values([
 		{ id: 1, text: 'Set up a dinner for friends', good: true, },
 		{ id: 2, text: 'Eat an entire blocki of cheese', good: false, },
-	]);
-}
+		]);
+		}
+		*/
