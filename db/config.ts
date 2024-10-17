7534c6943f86a9e5a0698c0f72f4184629dacf3d
Faddah Wolf <my_biz@me.com>
2024-10-16 19:40:39 -0700

    Refactored config.ts for DB to use Turso.

--- db/config.ts
+++ db/config.ts
@@ -1,5 +1,8 @@
-import { column, defineDb, defineTable } from 'astro:db';
+// import { defaultOptions } from './../node_modules/acorn/dist/acorn.d';
+import { turso } from '../src/turso';
+// import { column, defineDb, defineTable } from 'astro:db';
 
+/* 
 // https://astro.build/db/config
 export default defineDb({
 	tables: {
@@ -12,3 +15,19 @@ export default defineDb({
 		}),
 	}
 });
+ */
+
+// Write and exported turso.execute() function that deinfes a new table called Idea with the three following columns: id, text, and good. The id column should be a primary key, and the text column should be a text column. The good column should be a boolean column.
+export async function createIdeaTable() {
+	await turso.execute(`
+		CREATE TABLE IF NOT EXISTS Idea (
+			id integer primary key,
+			text varchar(256) not null,
+			good BOOLEAN default true,
+		)
+	`);
+	await turso.execute(`
+		create unique index idx_idea_id on Idea(id);
+	`);
+}
+
